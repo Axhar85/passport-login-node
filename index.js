333e4d5386182cb8094d3f252b5c95ffeffe1434
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-04 12:02:17 +0100

    created local verable

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
+const users = [];
+
 app.set('view-engine', 'ejs');
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name:'Aldo'})
@@ -10,6 +13,9 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login.ejs')
 });
+app.post('/login', (req, res) => {
+
+})
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
